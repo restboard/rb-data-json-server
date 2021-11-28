fe411ebb64bfee11553acd0229423655b9101169
Emanuele Bertoldi <emanuele.bertoldi@gmail.com>
2021-11-29 00:00:32 +0100

    feat: add request batching

--- src/index.js
+++ src/index.js
@@ -11,10 +11,7 @@ function _renderQuerystring (filters, sort, order, offset, limit) {
       let values = [filterValue]
       if (Array.isArray(filterValue)) {
         values = filterValue
-      } else if (
-        filterValue !== null &&
-        typeof filterValue === 'object'
-      ) {
+      } else if (filterValue !== null && typeof filterValue === 'object') {
         values = Object.keys(filterValue).filter(val => !!filterValue[val])
       }
       values.forEach(val => params.push(`${key}=${val}`))
@@ -37,15 +34,18 @@ function _renderQuerystring (filters, sort, order, offset, limit) {
 }
 
 class RbDataProviderJsonServer extends RbDataProvider {
-  constructor (apiURL, {
-    timeout,
-    retries,
-    backoff,
-    client,
-    tokenGetter,
-    responseParser,
-    querystringRenderer
-  } = {}) {
+  constructor (
+    apiURL,
+    {
+      timeout,
+      retries,
+      backoff,
+      client,
+      tokenGetter,
+      responseParser,
+      querystringRenderer
+    } = {}
+  ) {
     super()
     this.apiURL = apiURL
     this.timeout = timeout || 5000
@@ -55,21 +55,23 @@ class RbDataProviderJsonServer extends RbDataProvider {
     this.parseResponse = responseParser || (res => res.data || res)
     this.renderQuerystring = querystringRenderer || _renderQuerystring
     this.client = client || defaultClient
+    this.runningReqs = new Map()
   }
 
-  async getMany (resource, {
-    filters = {},
-    sort = '',
-    order = '',
-    offset = 0,
-    limit = null
-  } = {}) {
+  async getMany (
+    resource,
+    { filters = {}, sort = '', order = '', offset = 0, limit = null } = {}
+  ) {
     const base = `${this.apiURL}/${resource}`
     const qs = this.renderQuerystring(filters, sort, order, offset, limit)
-    const url = [base, qs].filter((v) => v).join('?')
-    const res = await this._performRequest(url, {
-      method: 'GET'
-    }, this.retries)
+    const url = [base, qs].filter(v => v).join('?')
+    const res = await this._performRequest(
+      url,
+      {
+        method: 'GET'
+      },
+      this.retries
+    )
     return {
       data: this.parseResponse(res)
     }
@@ -77,9 +79,13 @@ class RbDataProviderJsonServer extends RbDataProvider {
 
   async getOne (resource, { id }) {
     const url = `${this.apiURL}/${resource}/${id}`
-    const res = await this._performRequest(url, {
-      method: 'GET'
-    }, this.retries)
+    const res = await this._performRequest(
+      url,
+      {
+        method: 'GET'
+      },
+      this.retries
+    )
     return {
       data: this.parseResponse(res)
     }
@@ -88,10 +94,14 @@ class RbDataProviderJsonServer extends RbDataProvider {
   async createOne (resource, data) {
     const { id, ...attrs } = data
     const url = `${this.apiURL}/${resource}`
-    const res = await this._performRequest(url, {
-      method: 'POST',
-      body: attrs
-    }, this.retries)
+    const res = await this._performRequest(
+      url,
+      {
+        method: 'POST',
+        body: attrs
+      },
+      this.retries
+    )
     return {
       data: this.parseResponse(res)
     }
@@ -99,10 +109,14 @@ class RbDataProviderJsonServer extends RbDataProvider {
 
   async updateOne (resource, { id, ...data }) {
     const url = `${this.apiURL}/${resource}/${id}`
-    const res = await this._performRequest(url, {
-      method: 'PATCH',
-      body: data
-    }, this.retries)
+    const res = await this._performRequest(
+      url,
+      {
+        method: 'PATCH',
+        body: data
+      },
+      this.retries
+    )
     return {
       data: this.parseResponse(res)
     }
@@ -110,10 +124,14 @@ class RbDataProviderJsonServer extends RbDataProvider {
 
   async updateMany (resource, data) {
     const url = `${this.apiURL}/${resource}`
-    const res = await this._performRequest(url, {
-      method: 'PATCH',
-      body: data
-    }, this.retries)
+    const res = await this._performRequest(
+      url,
+      {
+        method: 'PATCH',
+        body: data
+      },
+      this.retries
+    )
     return {
       data: this.parseResponse(res)
     }
@@ -121,9 +139,13 @@ class RbDataProviderJsonServer extends RbDataProvider {
 
   async deleteOne (resource, { id }) {
     const url = `${this.apiURL}/${resource}/${id}`
-    await this._performRequest(url, {
-      method: 'DELETE'
-    }, this.retries)
+    await this._performRequest(
+      url,
+      {
+        method: 'DELETE'
+      },
+      this.retries
+    )
     return {
       data: { id }
     }
@@ -136,28 +158,45 @@ class RbDataProviderJsonServer extends RbDataProvider {
       Authorization: _token && `Bearer ${_token}`,
       ...options.headers
     }
-    const res = await this.client(url, {
+    const _reqOpts = {
       timeout: this.timeout,
       ...options,
       headers: _headers
+    }
+    const _reqId = JSON.stringify({
+      ..._reqOpts,
+      url
     })
-    if (!res.ok) {
+
+    // Batching
+    if (this.runningReqs.has(_reqId)) {
+      return this.runningReqs.get(_reqId)
+    }
+    const req = this.client(url, _reqOpts).finally(() =>
+      this.runningReqs.delete(_reqId)
+    )
+    this.runningReqs.set(_reqId, req)
+
+    // Retry on failure
+    return req.then(res => {
+      if (res.ok) {
+        return res.json()
+      }
       if (retries > 1 && retryCodes.includes(res.status)) {
         return new Promise((resolve, reject) => {
-          setTimeout(async () => {
-            try {
-              const res = await this._performRequest(url, options, retries - 1, _backoff * 2)
-              resolve(res)
-            } catch (err) {
-              reject(err)
-            }
-          }, _backoff)
+          const retryRequest = () => {
+            this._performRequest(url, options, retries - 1, _backoff * 2)
+              .then(resolve)
+              .catch(reject)
+          }
+          setTimeout(retryRequest, _backoff)
         })
       } else {
-        throw new Error(res.statusText)
+        throw new Error(
+          res.statusText || `request failed with status ${res.status}`
+        )
       }
-    }
-    return res.json()
+    })
   }
 }
 
---
+++ test/batching.test.js
@@ -0,0 +1,33 @@
+import * as t from 'tap'
+import createProvider from '../src/index'
+
+t.test('batching', async t => {
+  let attempts = 0
+  const successRes = {
+    status: 200,
+    statusText: 'OK',
+    ok: true,
+    json: async () => ({})
+  }
+  const fakeHttp = async () => {
+    attempts += 1
+    return new Promise((resolve, reject) => {
+      setTimeout(() => resolve(successRes), 250)
+    })
+  }
+  const opts = {
+    client: fakeHttp
+  }
+  const provider = createProvider('https://example.com', opts)
+  try {
+    await Promise.all([
+      provider.getMany('posts'),
+      provider.getMany('posts'),
+      provider.getMany('posts')
+    ])
+    t.equal(attempts, 1, 'should have batched requests in a single HTTP call')
+  } catch (err) {
+    console.error(err)
+    t.error(err, 'should not throw any error')
+  }
+})
